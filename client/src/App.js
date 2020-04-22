ac717271be3aa65fb3a47fab5ce52d225c4d5983
Sia <ashkiani@yahoo.com>
2020-04-22 19:59:19 -0400

    Updated the main/App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Nav from "./components/NavBar";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Redirect exact from="/" to="/about" />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          {/* <Route path="/admin" component={withAuth(Admin)} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
