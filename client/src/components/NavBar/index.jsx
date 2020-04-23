e2fce5365c3ef5fee7f8750c13f29cd87b35ea44
Sia <ashkiani@yahoo.com>
2020-04-22 21:31:50 -0400

    minor bug fix: replaced class with className in Navbar component

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -4,16 +4,16 @@ import { NavLink } from 'react-router-dom';
 export default function index() {
     return (
         <header>
-      <div class="container">
-        <nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center  bg-info text-white my-2">
-          <div class="p-2 w-90">
+      <div className="container">
+        <nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center  bg-info text-white my-2">
+          <div className="p-2 w-90">
             <h2>Siavash Ashkiani</h2>
           </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="navbar-collapse collapse w-10" id="collapsingNavbar3">
-            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+          <div className="navbar-collapse collapse w-10" id="collapsingNavbar3">
+            <ul className="nav navbar-nav ml-auto w-100 justify-content-end">
               <NavLink className="nav-item" to="/">
                 {/* <a className="nav-link" href="#">About Me </a> */}
                 <button type="button" className="btn btn-link text-light">About Me</button>
